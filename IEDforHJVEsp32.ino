7e7e25bfe9e70d9ed934c56e7c8626c9ae224955
Thor Lundsgaard Schierbeck <thor@schier-lunds.dk>
2023-01-27 17:01:44 +0100

    GPS Changes

--- IEDforHJVEsp32.ino
+++ IEDforHJVEsp32.ino
@@ -178,37 +178,37 @@ void setup_OTA()
    // Password can be set with it's md5 value as well
    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-    ArduinoOTA
-        .onStart([]() {
-        String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-    else // U_SPIFFS
-        type = "filesystem";
+    ArduinoOTA.onStart([]()
+            {
+            String type;
+                if (ArduinoOTA.getCommand() == U_FLASH)
+                    type = "sketch";
+                else // U_SPIFFS
+                    type = "filesystem";
 
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-            })
-        .onEnd([]() {
-                Serial.println("\nEnd");
-            })
-                .onProgress([](unsigned int progress, unsigned int total) {
-                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-                    })
-                .onError([](ota_error_t error) {
-                        Serial.printf("Error[%u]: ", error);
-                    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-                    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-                    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-                    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-                    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-                    });
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                Serial.println("Start updating " + type);
+           }).onEnd([]() 
+               {
+                    Serial.println("\nEnd");
+               }).onProgress([](unsigned int progress, unsigned int total) 
+                   {
+                    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+                   }).onError([](ota_error_t error) 
+                       {
+                            Serial.printf("Error[%u]: ", error);
+                            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+                      });
 
-                    ArduinoOTA.begin();
+    ArduinoOTA.begin();
 
-                    Serial.println("Ready");
-                    Serial.print("IP address: ");
-                    Serial.println(WiFi.localIP());
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 
@@ -219,6 +219,8 @@ void loop() {
         ArduinoOTA.handle();
         server.handleClient();
     }
+
+    // put your main code here, to run repeatedly:
     if (MotionDetected == true && digitalRead(DETECTOR_PIN) == LOW) 
     {
         runones = false;
@@ -253,6 +255,15 @@ void loop() {
         runones = false;
 
     }
+   
+    ReadGPS();
+ 
+    
+    
+}
+
+void ReadGPS()
+{
     while (Serial2.available() > 0)
     {
         if (gps.encode(Serial2.read()))
@@ -260,11 +271,7 @@ void loop() {
             displayInfo();
         }
     }
-   /* while (Serial2.available() > 0)
-        if (gps.encode(Serial2.read()))
-            displayInfo();*/
-    
-    // put your main code here, to run repeatedly:
+
 }
 
 void InitSensor()
